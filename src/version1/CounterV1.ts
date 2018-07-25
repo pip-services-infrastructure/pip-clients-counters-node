39edfec5fd16a239c5207e5654111e81aad64a61
Dmitriy Krayniy <krdima92@gmail.com>
2018-07-25 17:44:09 +0300

    added cache perfmon

--- src/version1/CounterV1.ts
+++ src/version1/CounterV1.ts
@@ -1,12 +1,23 @@
 import { CounterType } from 'pip-services-commons-node';
 
 export class CounterV1 {
-    public constructor(name: string, type: CounterType) {
+    public constructor(name: string, source:
+        string, type: CounterType, last: number,
+        count: number, min: number, max: number,
+        average: number) {
+        this.time = new Date();
         this.name = name;
         this.type = type;
+        this.source = source;
+        this.last = last;
+        this.count = count;
+        this.min = min;
+        this.max = max;
+        this.average = average;
     }
 
     public name: string;
+    public source: string;
     public type: CounterType;
     public last: number;
     public count: number;
